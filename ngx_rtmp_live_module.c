3c68c166b5014e7fc9ec7925314ed59418109090
Andras Elso <ea333@freemail.hu>
2012-07-19 20:45:32 +0200

    use delete event instead of close at live module

--- ngx_rtmp_live_module.c
+++ ngx_rtmp_live_module.c
@@ -10,7 +10,7 @@
 
 static ngx_rtmp_publish_pt              next_publish;
 static ngx_rtmp_play_pt                 next_play;
-static ngx_rtmp_close_stream_pt         next_close_stream;
+static ngx_rtmp_delete_stream_pt        next_delete_stream;
 
 
 static ngx_int_t ngx_rtmp_live_postconfiguration(ngx_conf_t *cf);
@@ -223,7 +223,7 @@ ngx_rtmp_live_join(ngx_rtmp_session_t *s, u_char *name,
 
 
 static ngx_int_t
-ngx_rtmp_live_close_stream(ngx_rtmp_session_t *s, ngx_rtmp_close_stream_t *v)
+ngx_rtmp_live_delete_stream(ngx_rtmp_session_t *s, ngx_rtmp_delete_stream_t *v)
 {
     ngx_rtmp_live_ctx_t            *ctx, **cctx;
     ngx_rtmp_live_stream_t        **stream;
@@ -282,7 +282,7 @@ ngx_rtmp_live_close_stream(ngx_rtmp_session_t *s, ngx_rtmp_close_stream_t *v)
     ctx->stream = NULL;
 
 next:
-    return next_close_stream(s, v);
+    return next_delete_stream(s, v);
 }
 
 
@@ -526,8 +526,8 @@ ngx_rtmp_live_postconfiguration(ngx_conf_t *cf)
     next_play = ngx_rtmp_play;
     ngx_rtmp_play = ngx_rtmp_live_play;
 
-    next_close_stream = ngx_rtmp_close_stream;
-    ngx_rtmp_close_stream = ngx_rtmp_live_close_stream;
+    next_delete_stream = ngx_rtmp_delete_stream;
+    ngx_rtmp_delete_stream = ngx_rtmp_live_delete_stream;
 
     return NGX_OK;
 }
